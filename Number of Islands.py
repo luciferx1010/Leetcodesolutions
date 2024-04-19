ddb777ae0ec7e290091f3d73227475f05a0002ae
Atharv <atharvpuranik15@gmail.com>
2024-04-19 20:14:40 +0530

    added code

--- Number of Islands.py
+++ Number of Islands.py
@@ -4,7 +4,7 @@ class Solution:
             return 0
         
         directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        num_islands = 0
+        num_islands = 0 #
         
         for i in range(len(grid)):
             for j in range(len(grid[0])):
