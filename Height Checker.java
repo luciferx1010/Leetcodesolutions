5bad60aefdc98ff0e7b6ea4b6fcc11717d7ad040
Atharv <atharvpuranik15@gmail.com>
2024-06-10 16:38:45 +0530

    solved todays code...

---
+++ Height Checker.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        int n = heights.length;
+        int count = 0;
+        // int expected[] = heights.clone();
+        int expected[] = new int[n];
+        for(int i=0; i<n; i++){
+            expected[i] = heights[i];
+        }
+        Arrays.sort(expected);
+        for(int i=0; i<n; i++){
+            if(heights[i] != expected[i]) count++;
+        }
+        return count;
+    }
+}
